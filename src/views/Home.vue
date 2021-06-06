829316193043e5b7607094078e436b3d11d2f34a
DomiiBunni <domi.jadowska@gmail.com>
2021-06-06 18:54:20 +0200

    Fix BG Image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,5 @@
 <template>
-  <div class="landing">
-    <div class="content">
-      <div class="holder">
-        <div class="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus magni
-          voluptatum quis nihil reprehenderit pariatur maxime odit numquam
-          dolorum provident! Dolore veritatis quae inventore explicabo natus
-          blanditiis laborum deserunt autem!
-        </div>
-        <div class="actions">
-          <div class="btn">Find out more!</div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div class="landing"></div>
 </template>
 
 <script>
@@ -22,28 +8,7 @@ export default {};
 
 <style lang="scss" scoped>
 .landing {
-  background-image: url(/bg.jpg);
+  background-image: url("/assets/bg.jpg");
   background-size: 100% 100%;
-  .content {
-    background-color: rgba(0, 0, 0, 0.5);
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    .holder {
-      width: 50%;
-      background-color: white;
-      height: 400px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: row;
-      div {
-        width: 100%;
-        text-align: center;
-      }
-    }
-  }
 }
 </style>
